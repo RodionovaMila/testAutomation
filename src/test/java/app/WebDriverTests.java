8f651d356f1bed75c8dd91f37a33eeda5b788ebe
Liudmyla Rodionova <rodionovalyda64@gmail.com>
2023-03-30 21:54:19 +0300

    WebDriverTests

---
+++ src/test/java/app/WebDriverTests.java
@@ -0,0 +1,44 @@
+package app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WebDriverTests {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void init(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testGetUrl1() {
+        driver.get("https://watatsumi.com.ua/");
+        WebElement loginRegistration = driver.findElement(By.cssSelector("#menu-item-106 > a"));
+        Assertions.assertTrue(loginRegistration.isDisplayed());
+    }
+
+    @Test
+    public void testGetUrl() {
+        driver.get("https://watatsumi.com.ua/my-account/");
+        WebElement registration = driver.findElement(By.xpath("//*[@id='reg_email']"));
+        registration.sendKeys("watatsumi@gmail.com");
+        Assertions.assertTrue(registration.isDisplayed());
+    }
+
+    @Test
+    public void testGetUrl2() {
+        driver.get("https://watatsumi.com.ua/kontakty");
+        WebElement kontakty = driver.findElement(By.cssSelector(".map"));
+        Assertions.assertTrue(kontakty.isDisplayed(), "Contact page not loaded");
+    }
+}
